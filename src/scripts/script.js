614830f061ce7909d4814d311e2e12eee688f54b
Owen Li <owen2000719lee@gmail.com>
2025-02-25 03:13:38 -0500

    feat: activate adpater and device on the bowser

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,11 @@
-console.log("hey");
+async function main() {
+
+  // Activate the gpu device on the bowser
+  const adapter = await navigator.gpu?.requestAdapter();
+  const device = await adapter?.requestDevice();
+  if (!device) {
+    fail("need a browser that supports WebGPU");
+    return;
+  }
+}
+main();
