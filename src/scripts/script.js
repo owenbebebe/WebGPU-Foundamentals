beb281de0b209f21ec7bfe36f538a041d20aea85
Owen Li <owen2000719lee@gmail.com>
2025-02-25 03:19:31 -0500

    feat: configure our canvas to webgpu

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -7,5 +7,14 @@ async function main() {
     fail("need a browser that supports WebGPU");
     return;
   }
+
+  // Binding canvas to our just created GPU device
+  const canvas = document.querySelector('canvas');
+  const context = canvas.getContext('webgpu');
+  const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
+  context.configure({
+    device,
+    format: presentationFormat,
+  });
 }
 main();
